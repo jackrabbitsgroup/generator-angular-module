ff2ee3eff98c7ca72c44bcff7c622c44705ed9ec
Luke Madera <luke.madera@gmail.com>
2013-11-03 13:36:52 -0800

    update _service.js structure

--- service/templates/gh-pages/_service.js
+++ service/templates/gh-pages/_service.js
@@ -9,9 +9,12 @@
 
 angular.module('<%= githubName %>.<%= moduleName %>', [])
 .factory('<%= modulePrefix%><%= _.capitalize(_.camelize(moduleNamePart)) %>', [ function () {
+
+	//public methods & properties
+	var self ={
+	};
+	
 	//private methods and properties - should ONLY expose methods and properties publicly (via the 'return' object) that are supposed to be used; everything else (helper methods that aren't supposed to be called externally) should be private.
 	
-	//public methods & properties
-	return {
-	};
+	return self;
 }]);
